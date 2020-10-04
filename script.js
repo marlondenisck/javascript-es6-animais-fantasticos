7463a8d716cf602277f2acc76709bc04b2ad8d2e
marlondck <15050029+marlondck@users.noreply.github.com>
2020-10-04 00:02:58 -0400

    Feat: Scroll Suave

--- script.js
+++ script.js
@@ -71,3 +71,34 @@ function initAccordion() {
 }
 
 initAccordion();
+
+/** Scrool suave */
+function initScrollSuave() {
+  // 1º  Seleciona os links que tenham href="#algumaSection"
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  //3º evento
+  function scrollToSection(event) {
+    //3.1 previne comportamento padrao de scroll
+    event.preventDefault();
+    
+    //3.2 pega o link só com com a #algumaSection
+    const href = event.currentTarget.getAttribute('href');
+
+    // 3.3 Seleciona a section que contenha o id do parametro passado
+    const section = document.querySelector(href);
+    
+    // 3.4 Realizar scroll
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
+
+  // 2º para cada link da nodelist crie um evento 
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+
+initScrollSuave();
